fb252edb1cf038274cd8f9774bfe4c820d04365a
Jonhathan HO TZU YU <hotzuyu@hotzuyu-graphic.fr>
2023-03-02 16:07:17 +0100

    add a reducer push in state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Header from './components/Header';
 import Home from './pages/Home';
 import Current from './pages/Current';
 import Error404 from './pages/Error404';
 import Footer from './components/Footer';
+import store from './utils/redux/store';
 
 import './App.css';
 
 function App() {
   return (
     <React.StrictMode>
-            
+            <Provider store={store} >
             <Router>
                 <Header />
                 <Routes>
@@ -22,6 +24,7 @@ function App() {
                 </Routes>
                 <Footer />
             </Router>
+            </Provider>
         </React.StrictMode>
   );
 }
