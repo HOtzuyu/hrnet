e830d93432b2fd81167631976180e316170820e4
Jonhathan HO TZU YU <hotzuyu@hotzuyu-graphic.fr>
2023-03-14 11:03:58 +0100

    visual css

--- src/pages/Current.jsx
+++ src/pages/Current.jsx
@@ -1,19 +1,27 @@
 import { DataTable } from "@lephenix47/react-datatable";
 import { useSelector } from "react-redux";
 import { selectEmployees } from "../utils/redux/selector";
+import mockedUser from "../data/mockup";
 
 function Current() {
   const employees = useSelector(selectEmployees).dataEmployee;
 
+  const dataEmployees = mockedUser;
+  //const datas = employees.push.apply(employees, dataEmployees)
+
   return (
+    <>
+    {console.log(employees)}
+    {console.log(dataEmployees)}
+    <h1 className="current-title">List of Employees</h1>
     <DataTable
-      data={employees}
+      data={dataEmployees}
       paging={true}
       sort={true}
       filter={true}
       info
-      title={"List of employees"}
-    />
+      />
+    </>
   );
 }
 
